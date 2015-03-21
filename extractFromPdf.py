7fa128bf4ce622715425be75cd017ac91a60436f
enlighter <enlighter.1992@gmail.com>
2015-03-22 01:10:22 +0530

    create a basic metadata extractor

---
+++ extractFromPdf.py
@@ -0,0 +1,24 @@
+#Run the following command from your shell before running this python code file
+#sudo -E pip install --upgrade --ignore-installed pdfminer==20140328 or higher version
+
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfpage import PDFTextExtractionNotAllowed
+
+# Open a PDF file.
+fp = open('sample.pdf', 'rb')
+# Create a PDF parser object associated with the file object.
+parser = PDFParser(fp)
+# Create a PDF document object that stores the document structure.
+# Supply the password for initialization.
+document = PDFDocument(parser)
+# Check if the document allows text extraction. If not, abort.
+if not document.is_extractable:
+    raise PDFTextExtractionNotAllowed
+#parser.set_document(doc)
+#doc.set_parser(parser)
+#document.initialize()
+
+print document.info # The "Info" metadata
+#print document.catalog
