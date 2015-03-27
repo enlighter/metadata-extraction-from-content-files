bff447f68c521707bddbc03f58104fb0f2e03bb5
enlighter <enlighter.1992@gmail.com>
2015-03-27 17:43:21 +0530

    make the code more portable

--- extractFromEpub.py
+++ extractFromEpub.py
@@ -14,27 +14,27 @@ def get_epub_info(fname):
 
     # find the contents metafile
     txt = zip.read('META-INF/container.xml')
-    print txt
+    #print(txt)
     tree = etree.fromstring(txt)
     cfname = tree.xpath('n:rootfiles/n:rootfile/@full-path',namespaces=ns)[0]
-    #print cfname
+    print(cfname)
 
     # grab the metadata block from the contents metafile
     cf = zip.read(cfname)
     tree = etree.fromstring(cf)
-    #print tree
+    print( etree.tostring(tree, pretty_print=True) )
     p = tree.xpath('/pkg:package/pkg:metadata',namespaces=ns)[0]
-    #print p
+    #print(p)
 
     # repackage the data
     res = {}
     for s in dataterms.DataTerms:
         #check if the element exists before extracting 1st element from the list
         Xpath = p.xpath('dc:%s/text()'%(s),namespaces=ns)
-        if Xpath:
+        if len(Xpath):
             print Xpath
             res[s] = Xpath[0]
 
     return res
 
-print get_epub_info("sample.epub")
+print(get_epub_info("sample.epub"))
