a722bf904b9ee635b1d68ce3d3ec0eeddc3bc728
enlighter <mandal.sushovan92@gmail.com>
2015-04-23 10:02:26 +0530

    refactor project based on new dependencies

--- extractFromEpub.py
+++ extractFromEpub.py
@@ -1,6 +1,7 @@
 ''' sudo -E pip install epubzilla
 	sudo -E pip install -U epubzilla (upgrade epubzilla and dependencies if already installed)
-	[make sure you have python-dev packages installed in your system ,such as python-dev, python-all-dev, python2.7-dev et al]
+	[In Ubuntu Systems make sure you have python-dev packages installed in your system ,such as 
+	python-dev, python-all-dev, python2.7-dev et al]
 
 	__author__: "Sushovan Mandal"
 	__license__: "GPLv2"
@@ -10,25 +11,28 @@
 '''
 
 #from epubzilla.epubzilla import Files
-from epubzilla.epubzilla import Epub
+#from epubzilla.epubzilla import Epub
+from dependencies.epubzilla.epubzilla.epubzilla import Epub
 from lxml import etree
 import dataterms
 
 def get_epub_info(filename):
 
 	epub = Epub.from_file(filename)
-	for element in epub.metadata:
-		print("%s : %s")%(element.tag.localname, element.tag.text)
-		for k,v in element.tag.iteritems():
-			print("\t %s : %s")%(k,v)
+	#print(epub.metadata.dublin_core_elements)
+	metadata = epub.metadata
+	for localname in metadata.dublin_core_elements:
+		print("%s : %s")%(localname, metadata.get(localname))
+		#for key, value in element.attributes.iteritems():
+		#	print("\t %s : %s")%(key, value)
 
 	print(epub.author)
 
 	for item in epub.manifest:
-		if item.tag.attributes['id'] == dataterms.toc_ncx_id:
+		if item.attributes['id'] == dataterms.toc_ncx_id:
 			print("got ncx")
 			toc_ncx = item.get_file()
-			print(toc_ncx)
+			#print(toc_ncx)
 			toc_tree = etree.fromstring(toc_ncx)
 			print(toc_tree)
 			break
