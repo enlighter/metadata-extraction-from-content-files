2616ecb5d30c3bf3c55e0881332a3305f0f70011
enlighter <mandal.sushovan92@gmail.com>
2015-05-04 08:49:00 +0530

    create main executer file

---
+++ extractor.py
@@ -0,0 +1,14 @@
+''' [In Ubuntu based systems make sure you have python-dev packages installed in your system,
+	such as python-dev, python-all-dev, python3-dev et al]
+	sudo [-E] pip install pprintpp
+	sudo [-E] pip install beautifulsoup4
+	In case of python2 as system default, use pip3 instead
+
+	__author__: "Sushovan Mandal"
+	__license__: "GPLv2"
+	__email__: "mandal.sushovan92@gmail.com"
+
+	use python >= 3.4
+	use ebooklib=0.15
+'''
+#!/usr/bin/python3
